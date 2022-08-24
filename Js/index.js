3f39d5c1e72084c255425a391e3a2a9c32fe8e9e
Morshaedul <morshaedulanirbanit@gmail.com>
2022-08-24 22:41:53 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,15 @@
+let x = 5;
+let y = 2;
+let z = x % y;
+document.getElementById("demo").innerHTML = z;
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
